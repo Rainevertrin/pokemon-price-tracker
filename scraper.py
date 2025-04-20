3d43c3174d103a07828e022a675defc38ce08a0f
Rainevertrin <vertigca@gmail.com>
2025-04-20 22:58:03 +0200

    add flask, logging to req and add flask to scraper.py

--- scraper.py
+++ scraper.py
@@ -1,4 +1,21 @@
+import os
+from flask import Flask
+import logging
+
 from update_gsheet import update_sheet
 
-if __name__ == "__main__":
+def run_scraper():
+    logging.info("Running update_sheet...")
     update_sheet()
+    logging.info("Finished update_sheet.")
+
+if __name__ == "__main__":
+    run_scraper()
+
+    app = Flask(__name__)
+    @app.route("/")
+    def home():
+        return "Pokemon Price Tracker is running!"
+
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
