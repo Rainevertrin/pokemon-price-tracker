8ea6b9b74caf700c8fb9080df49183ccae4b680a
Rainevertrin <vertigca@gmail.com>
2025-04-22 15:58:37 +0200

    fix for cloudbuild

--- tcg_scraper.py
+++ tcg_scraper.py
@@ -16,12 +16,13 @@ logging.basicConfig(level=logging.INFO)
 class TCGScraper:
     def __init__(self):
         options = webdriver.ChromeOptions()
-        options.add_argument("--headless=new")
+        options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--disable-gpu")
         options.add_argument("--window-size=1920,1080")
         options.add_argument("--blink-settings=imagesEnabled=false")
+        options.add_argument("--log-level=DEBUG")
         options.page_load_strategy = "eager"
 
         capabilities = DesiredCapabilities.CHROME.copy()
